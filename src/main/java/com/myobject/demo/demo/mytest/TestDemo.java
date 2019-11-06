a5bf1030c5a83511c23b7d22b5c08bed0c405676
shengwen-xue <xueshengwen@aliyun.com>
2019-11-06 17:13:33 +0800

    提交项目

---
+++ src/main/java/com/myobject/demo/demo/mytest/TestDemo.java
@@ -0,0 +1,13 @@
+package com.myobject.demo.demo.mytest;
+
+/**
+ * Create by
+ *
+ * @author xueshengwen
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/myobject/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myobject.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
